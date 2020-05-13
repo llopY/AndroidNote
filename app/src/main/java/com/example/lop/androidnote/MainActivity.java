efc75d3b45d7a80f4afa7e92ad9e6a2fa9cb7687
lop <halalop@163.com>
2020-05-13 18:52:49 +0800

    一些常用的三方库

--- app/src/main/java/com/example/lop/androidnote/MainActivity.java
+++ app/src/main/java/com/example/lop/androidnote/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.lop.androidnote;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
     }
 }
